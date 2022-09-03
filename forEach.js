e6255a7c5300d5e5553ad394a3a0849cf49eb076
CedrimMCZ <104163929+CedrimMCZ@users.noreply.github.com>
2022-09-03 18:31:40 -0300

    Create forEach.js

---
+++ forEach.js
@@ -0,0 +1,31 @@
+```
+const fruits = ['banana', 'morango', 'manga', 'abacate']
+
+function eatAllFruits () {
+
+ fruits.forEach ((fruit, index) => {
+ console. log(`huuum acabei de comer ${fruit}`)
+
+ console. log(`ela era o ${index +1} da minha lista`)
+})
+}
+
+// expected output: 
+huuum acabei de comer banana
+ela era o 1 da minha lista
+
+// expected output: 
+huuum acabei de comer morango
+ela era o 2 da minha lista
+...
+```
+
+```
+const array1 = ['a', 'b', 'c'];
+
+array1.forEach(x => console.log(x));
+
+// expected output: "a"
+// expected output: "b"
+// expected output: "c"
+```
